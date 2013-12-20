60a7808216c82c95c09f29156a35874575a62c9c
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2013-12-20 18:25:24 +0100

    Fixed PEP8 issues

--- ckanext/zhstat/plugins.py
+++ ckanext/zhstat/plugins.py
@@ -1,8 +1,8 @@
-import ckan
 import ckan.plugins as p
-from pylons import config
+
 
 class ZhstatHarvest(p.SingletonPlugin):
     """
-    Plugin containing the harvester for for the Statistical Office of Canton of Zurich
+    Plugin containing the harvester for for the
+    Statistical Office of Canton of Zurich
     """
